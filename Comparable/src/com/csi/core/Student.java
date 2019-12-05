88e1fb842643f135d16602adf3c3294fd3fa8d1f
Lenovo <Lenovo@DESKTOP-N0I1SF6.Home>
2019-12-06 00:10:17 +0530

    comparable demo

---
+++ Comparable/src/com/csi/core/Student.java
@@ -0,0 +1,36 @@
+package com.csi.core;
+
+public class Student implements Comparable<Student>{
+private int rollno;
+private int age;
+private String name;
+public int getRollno() {
+	return rollno;
+}
+public void setRollno(int rollno) {
+	this.rollno = rollno;
+}
+public int getAge() {
+	return age;
+}
+public void setAge(int age) {
+	this.age = age;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public Student(int rollno, int age, String name) {
+	super();
+	this.rollno = rollno;
+	this.age = age;
+	this.name = name;
+}
+@Override
+public int compareTo(Student s) {
+	return name.compareTo(s.name);
+}
+
+}
